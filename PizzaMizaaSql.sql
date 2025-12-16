223de6e53ca21a58f85d62b165d8a151661bc0ff
Seyidakif101 <sayidakhifia-mpa101@code.edu.az>
2025-12-16 23:15:03 +0400

    Delete PizzaMizaaSql.sql

--- PizzaMizaaSql.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE PizzaMizzaDB
-USE PizzaMizzaDB
-drop table Pizzas 
-CREATE TABLE Pizzas (
-    Id INT PRIMARY KEY IDENTITY,
-    Name NVARCHAR(100) not null ,
-    Price DECIMAL(6,2) not null ,
-    IngredientCount INT not null 
-)
